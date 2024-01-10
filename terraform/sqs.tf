0163d737036066a195a5cd11d73ca111121f0b21
camilomontoyau <camilo.montoya@techstars.com>
2024-01-10 15:52:41 -0500

    Add AWS provider and SQS queue resources

---
+++ terraform/sqs.tf
@@ -0,0 +1,8 @@
+resource "aws_sqs_queue" "my_queue" {
+  name                      = "my-queue"
+  delay_seconds             = 0
+  max_message_size          = 262144
+  message_retention_seconds = 345600
+  visibility_timeout_seconds = 30
+  receive_wait_time_seconds = 0
+}
