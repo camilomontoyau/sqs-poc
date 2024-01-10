0ff3b1fc348fcb5049f2cd2a015b48eb328410d7
camilomontoyau <camilo.montoya@techstars.com>
2024-01-10 16:15:53 -0500

    Add IAM roles for Cert Manager and External DNS

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,3 +1,11 @@
 output "sqs_queue_arn" {
   value = aws_sqs_queue.my_queue.arn
 }
+
+output "cert_manager_irsa_role" {
+  value = module.cert_manager_irsa_role.iam_role_arn
+}
+
+output "external_dns_irsa_role" {
+  value = module.external_dns_irsa_role.iam_role_arn
+}
