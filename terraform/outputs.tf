5cd42a927f7c6b8a676553c95de56e401b8d0f64
camilomontoyau <camilo.montoya@techstars.com>
2024-01-10 23:14:08 -0500

    Add output for SQS queue URL

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -2,6 +2,10 @@ output "sqs_queue_arn" {
   value = aws_sqs_queue.my_queue.arn
 }
 
+output "sqs_queue_url" {
+  value = aws_sqs_queue.my_queue.url
+}
+
 output "cert_manager_irsa_role" {
   value = module.cert_manager_irsa_role.iam_role_arn
 }
