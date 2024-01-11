3d1cd544a929c3b0e1094c0d0d4163273215cb64
camilomontoyau <camilo.montoya@techstars.com>
2024-01-10 21:21:03 -0500

    Update ECR repository name to use API name

--- terraform/ecr.tf
+++ terraform/ecr.tf
@@ -1,5 +1,5 @@
 resource "aws_ecr_repository" "my_repository" {
-  name = "${var.main_name}-repository"
+  name = var.api_name
   image_tag_mutability = "IMMUTABLE"
   image_scanning_configuration {
     scan_on_push = true
