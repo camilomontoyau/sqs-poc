4c604ec8ce7fd148271cfadbaefb8fc09de2e3c4
camilomontoyau <camilo.montoya@techstars.com>
2024-01-11 00:45:23 -0500

    Add available message count logging and early return if no messages available

--- api/src/process-messages.ts
+++ api/src/process-messages.ts
@@ -1,7 +1,12 @@
-import { readMessagesFromSQS } from "./sqs-service"
+import { readMessagesFromSQS, getAvailableMessagesCount } from "./sqs-service"
 
 
 const processMessages = async () => {
+  const availableMessagesCount = await getAvailableMessagesCount()
+  console.log(`There are ${availableMessagesCount} messages available to process`)
+  if (!availableMessagesCount) {
+    return
+  }
   const messages = await readMessagesFromSQS()
   console.log(messages)
 }
