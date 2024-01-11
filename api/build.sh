1782a541e5a5bee11e952cdc75c26a8eb901a582
camilomontoyau <camilo.montoya@techstars.com>
2024-01-11 00:14:15 -0500

    Add Dockerfile and ECR configuration for worker image

--- api/build.sh
+++ api/build.sh
@@ -23,9 +23,18 @@ echo "Building $BUILD_NAME"
 # build the Docker image
 docker build --platform linux/amd64 -t $BUILD_NAME .;
 
-
 # tag the image
 docker tag $BUILD_NAME "$4.dkr.ecr.$3.amazonaws.com/$BUILD_NAME"
 
 # push the image
 docker push "$4.dkr.ecr.$3.amazonaws.com/$BUILD_NAME"
+
+WORKER_BUILD_NAME="$1-worker:$2"
+# build the Docker image with Dockerfile.worker
+docker build --platform linux/amd64 -t $WORKER_BUILD_NAME -f Dockerfile.worker .;
+
+# tag the image
+docker tag $WORKER_BUILD_NAME "$4.dkr.ecr.$3.amazonaws.com/$WORKER_BUILD_NAME"
+
+# push the image
+docker push "$4.dkr.ecr.$3.amazonaws.com/$WORKER_BUILD_NAME"
--- terraform/ecr.tf
+++ terraform/ecr.tf
@@ -5,3 +5,11 @@ resource "aws_ecr_repository" "my_repository" {
     scan_on_push = true
   }
 }
+
+resource "aws_ecr_repository" "my_repository_worker" {
+  name = "${var.api_name}-worker"
+  image_tag_mutability = "IMMUTABLE"
+  image_scanning_configuration {
+    scan_on_push = true
+  }
+}
