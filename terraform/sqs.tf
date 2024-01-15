13a601dfc21fc1ab10abc9893c4a04947557eda1
camilomontoyau <camilo.montoya@techstars.com>
2024-01-14 22:05:41 -0500

    Add dependency on aws_sqs_queue.my_queue in aws_iam_policy.custom_sqs_policy

--- terraform/sqs.tf
+++ terraform/sqs.tf
@@ -8,6 +8,7 @@ resource "aws_sqs_queue" "my_queue" {
 }
 
 resource "aws_iam_policy" "custom_sqs_policy" {
+  depends_on = [ aws_sqs_queue.my_queue ]
   name        = "sqs"
   description = "A custom SQS policy"
   policy      = <<EOF
